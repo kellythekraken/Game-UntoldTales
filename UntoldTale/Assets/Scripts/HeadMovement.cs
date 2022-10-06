1cf71c17d99f7151b26a3411a9482ea1616d7c73
Kelly Liang <kyqliang@gmail.com>
2022-10-06 18:28:56 +0100

    adding input system
    -also added assembly reference
    -trying to make player input

--- UntoldTale/Assets/Scripts/HeadMovement.cs
+++ UntoldTale/Assets/Scripts/HeadMovement.cs
@@ -6,21 +6,42 @@ public class HeadMovement : MonoBehaviour
 {
     Camera mainCam;
     public float rotSpeed;
-    public float movepeed;
+    public float moveSpeed;
     private Vector2 direction;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
     void Start()
     {
         mainCam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        //direction = mainCam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        //float angle = Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg;
-        //Quaternion rotation = Quaternion.AngleAxis(angle,Vector3.forward);
-        //transform.rotation = Quaternion.Slerp(transform.rotation,rotation,rotSpeed * Time.deltaTime);
-        
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.deltaTime);
+        //MoveToCursor();
+    }
+
+    void OnMove()
+    {
+
+    }
+    void RotateToCursor()
+    {
+        direction = mainCam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle,Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation,rotation,rotSpeed * Time.deltaTime);
+    }
+
+    void MoveByInput()
+    {
+
+    }
+    void MoveToCursor()
+    {
         Vector2 cursorPos = mainCam.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = Vector2.MoveTowards(transform.position,cursorPos,movepeed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position,cursorPos,moveSpeed * Time.deltaTime);
     }
 }
