7712733ea522b7c967ba25f9867a7ccf2435fb72
Kelly Liang <kyqliang@gmail.com>
2022-10-08 23:34:41 +0100

    stamina system
    adding social battery feature

--- UntoldTale/Assets/Scripts/RangeDetection.cs
+++ UntoldTale/Assets/Scripts/RangeDetection.cs
@@ -5,31 +5,52 @@ using UnityEngine;
 public class RangeDetection : MonoBehaviour
 {
     [SerializeField] List<Befriendable> befriendList;
-    public float befriendSpeed = 1f;
+    public float befriendSpeed = 1f;    //could be modified as you become better at it
+    public float exhaustSpeed = 1f;      //will DOUBLE if you befriending multiple at once
+    public int exhaustRate = 0;         //how many you're socializing at once
+    public float rechargeSpeed = 1f;
+    public float socialBattery = 100f;   //reduce when you're befriending blobs
 
     void OnEnable() =>befriendList = new List<Befriendable>();
+
+    void Update()
+    {
+        if(exhaustRate>0) {
+            if(socialBattery>0f) socialBattery -= exhaustSpeed * exhaustRate * Time.deltaTime;
+        }
+        else{ 
+        if(socialBattery<100f) socialBattery += rechargeSpeed * Time.deltaTime;
+        }
+
+        if(socialBattery>0)
+        {
+            foreach(var i in befriendList) 
+            {
+                if(i.friendliness<100) i.friendliness += befriendSpeed * Time.deltaTime;
+                else
+                {
+                    i.befriended = true;
+                    befriendList.Remove(i);
+                    exhaustRate --;
+                }
+            }
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.CompareTag("FriendToBe")) 
         {
             befriendList.Add(collider.GetComponentInParent<Befriendable>());
-        }
-    }
-
-    void OnTriggerStay2D(Collider2D collider)
-    {
-        if(!collider.CompareTag("FriendToBe")) return;
-        foreach(var i in befriendList) 
-        {
-            if(i.friendliness<100) i.friendliness += befriendSpeed * Time.deltaTime;
-            else{
-                i.befriended = true;
-            }
+            exhaustRate ++;
         }
     }
 
     void OnTriggerExit2D(Collider2D collider)
     {
-        if(collider.CompareTag("FriendToBe")) befriendList.Remove(collider.GetComponentInParent<Befriendable>());
+        if(collider.CompareTag("FriendToBe")) 
+        {
+            befriendList.Remove(collider.GetComponentInParent<Befriendable>());
+            exhaustRate --;
+        }
     }
 }
