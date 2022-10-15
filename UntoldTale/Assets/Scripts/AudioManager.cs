bb6cbd33607f35222055a3c4dc3dc6ceb71aee4d
Kelly Liang <kyqliang@gmail.com>
2022-10-15 12:55:50 +0100

    import fmod

---
+++ UntoldTale/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource EffectsSource;
+	public AudioSource MusicSource;
+	// Random pitch adjustment range.
+	public float LowPitchRange = .95f;
+	public float HighPitchRange = 1.05f;
+	// Singleton instance.
+	public static AudioManager Instance = null;
+	
+	// Initialize the singleton instance.
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else if (Instance != this)
+		{
+			Destroy(gameObject);
+		}
+		//Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+		DontDestroyOnLoad (gameObject);
+	}
+	// Play a single clip through the sound effects source.
+	public void Play(AudioClip clip)
+	{
+		EffectsSource.clip = clip;
+		EffectsSource.Play();
+	}
+	// Play a single clip through the music source.
+	public void PlayMusic(AudioClip clip)
+	{
+		MusicSource.clip = clip;
+		MusicSource.Play();
+	}
+	// Play a random clip from an array, and randomize the pitch slightly.
+	public void RandomSoundEffect(params AudioClip[] clips)
+	{
+		int randomIndex = Random.Range(0, clips.Length);
+		float randomPitch = Random.Range(LowPitchRange, HighPitchRange);
+		EffectsSource.pitch = randomPitch;
+		EffectsSource.clip = clips[randomIndex];
+		EffectsSource.Play();
+	}
+}
